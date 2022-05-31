4a7f75079e5dc1e62216b9848213e81c29a8e1aa
Dillon Brock <mdillon1297@gmail.com>
2022-05-31 16:19:45 -0700

    added pronoun input and display

--- app.js
+++ app.js
@@ -3,9 +3,24 @@
 const nameInput = document.getElementById('name-input');
 const nameDisplay = document.getElementById('name-display');
 
+const pronounInput = document.getElementById('pronoun-input');
+const pronounDisplay = document.getElementById('pronoun-display');
+
 nameInput.addEventListener('input', () => {
     let name = nameInput.value;
     nameDisplay.textContent = name;
+});
+
+pronounInput.addEventListener('input', () => {
+    let pronouns = pronounInput.value;
+    if (pronouns) {
+        pronounDisplay.style.display = 'block';
+        pronounDisplay.textContent = pronouns;
+        nameDisplay.style.paddingBottom = '15px';
+    } else {
+        pronounDisplay.style.display = 'none';
+        nameDisplay.style.paddingBottom = '25px';
+    }
 });
 
 // set event listeners
