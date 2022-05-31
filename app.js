03f2bd4524ca4e50ca29c7b1a1c6a1a694a4b047
Dillon Brock <mdillon1297@gmail.com>
2022-05-31 15:39:14 -0700

    validation of name input

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // grab DOM elements
 
+const nameInput = document.getElementById('name-input');
+console.log(nameInput);
+
 // set event listeners
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
-
